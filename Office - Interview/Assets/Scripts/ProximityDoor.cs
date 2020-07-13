5223355799877d377336a3c9bc621e2d85970019
Thomas Huynh <tigereyez57@gmail.com>
2020-07-13 14:31:31 +0100

    finished the requiered criterias
    Made a proximity door.
    Made a tap door.
    Added more rooms so that the doors lead somewhere.
    Added lables above the doors.

---
+++ Office - Interview/Assets/Scripts/ProximityDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityDoor : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("isOpening", true);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("isOpening", false);
+    }
+}
---
+++ Office - Interview/Assets/Scripts/TapDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapDoor : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.transform.GetComponent<Animator>();
+    }
+
+    public void Open()
+    {
+        anim.SetBool("isOpening", true);
+    }
+
+    public void Close()
+    {
+        anim.SetBool("isOpening", false);
+    }
+
+}
